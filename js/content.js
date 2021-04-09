10d1e28a2cfe6285d0b843432b15dedf229e3f45
Jay <jay@thegraysun.com>
2021-04-09 08:03:01 -0400

    Added auto-accept for IAR Cookie Policy
    IAMRESPONDING added a cookie acceptance to their site recently and this addition will click the accept button before proceeding to the login page.

--- js/content.js
+++ js/content.js
@@ -5,20 +5,26 @@ chrome.runtime.sendMessage({function: "getDefaults"}, function(response) {
 	var password = response.password;
 
 	if (window.location.href.toLowerCase().match("/v3/pages/memberlogin.aspx")) {
+		
 		var agencyBox = document.querySelector('#ddlsubsciribers');
 		var userBox = document.querySelector('#memberfname');
 		var passBox = document.querySelector('#memberpwd');
 
-		//document.querySelector("#login").addEventListener('click', function () {
-
-		    	agencyBox.value = agency;
-				userBox.value = username;
-				passBox.value = password;
-
-		//},false);
+		agencyBox.value = agency;
+		userBox.value = username;
+		passBox.value = password;
 
 		document.querySelector("#login").click();
 
 	}
-	else if(window.location.href.toLowerCase().match('https://www.iamresponding.com/')) window.location.href = 'https://www.iamresponding.com/v3/Pages/memberlogin.aspx';
+	else if(window.location.href.toLowerCase().match('https://www.iamresponding.com/'))
+	{
+		if (document.getElementsByClassName("CookieConsent").length >= 1)
+		{
+			console.log(document.getElementsByClassName("CookieConsent"));
+			document.getElementsByClassName("CookieConsent")[0].getElementsByTagName('div')[1].getElementsByTagName('button')[0].click();
+		}
+
+		window.location.href = 'https://www.iamresponding.com/v3/Pages/memberlogin.aspx';
+	}
 });
